79974edb86585b4b3603c85ff706347e071cbc6a
Cheemsburb <sencilgladwyn@gmail.com>
2025-10-26 23:09:17 -0700

    Task 2 is done

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\TaskSeeder; // <-- ADD THIS LINE
 
 class DatabaseSeeder extends Seeder
 {
@@ -23,5 +24,8 @@ class DatabaseSeeder extends Seeder
         ]);
 
         $this->call(ProductSeeder::class);
+        $this->call(
+            TaskSeeder::class,
+        );
     }
 }
